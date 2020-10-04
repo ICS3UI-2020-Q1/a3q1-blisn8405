f139720f3c37f3842b9dbdeacc833c8886826307
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 11:17:36 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,21 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+   // create a scanner for user input
+Scanner input = new Scanner(System.in);    
+  // ask the user for 3 integers
+ System.out.println("Please enter 3 numbers");
+  int int1 = input.nextInt();
+  int int2 = input.nextInt();
+ int int3 = input.nextInt();
+  
+  if(int1 > int2 && int1 > int3){
+   System.out.println("Your maximum is " + int1);
+ }else if(int2 > int1 && int2 > int3){
+   System.out.println("Your maximum is " + int2);
+ }else if(int3 > int1 && int3 > int2){
+   System.out.println("Your maximum is " + int3);
+ }
   }
 }
+
